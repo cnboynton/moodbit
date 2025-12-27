1c0ee6980dcb6d3e9238d6a0467c98f4da4ae998
chettyyy <chester.boynton@gmail.com>
2025-12-27 14:42:13 -0500

    detect and replace keyword from hardcoded list

--- Moodbit/InputController.swift
+++ Moodbit/InputController.swift
@@ -8,18 +8,68 @@
 import Cocoa
 import InputMethodKit
 
+var sampleEmojis: [String:String] = [
+    ":fire":"🔥",
+    ":happy":"😆"
+]
+
+enum collectionState {
+    case idle, spaceDetected, live
+}
+
 @objc(InputController)
 class InputController: IMKInputController {
+
+    private var collectionState: collectionState = .idle
+    private var buffer: String = ""
+    private var selectedEmoji: String = ""
+    private var compStartLocation: Int = 0
+    private var compLength: Int = 0
+    
     override func inputText(_ string: String!, client sender: Any!) -> Bool {
-        NSLog(string)
+        guard let string = string else { return false }
         // get client to insert
-        guard let client = sender as? IMKTextInput else {
+        guard let client = sender as? IMKTextInput else { return false }
+        
+        // LIVE COLLECTION
+        if self.collectionState == .live {
+            // IF TERMINATED
+            if string == " " {
+                if sampleEmojis.keys.contains(buffer) { selectedEmoji = sampleEmojis[buffer]! }
+                else { selectedEmoji = buffer }
+                compLength = buffer.count
+                buffer = "" // clear buffer
+                client.insertText(selectedEmoji, replacementRange: NSRange(location: compStartLocation, length: compLength))
+                client.insertText(" ", replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+                self.collectionState = .spaceDetected
+                return true
+            }
+            buffer += string
+            NSLog(string)
             return false
         }
-        if string == ":" {
-            client.insertText(string+string, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
-            return true
-        } else {
+        // SPACE DETECTED
+        else if self.collectionState == .spaceDetected {
+            if string == ":" {
+                NSLog(string)
+                let selectedRange = client.selectedRange()
+                compStartLocation = selectedRange.location
+                self.collectionState = .live
+                buffer += string
+            }
+            else if string == " " || string == "\n" || client.selectedRange().location == 0 {
+                self.collectionState = .spaceDetected
+            }
+            else {
+                self.collectionState = .idle
+            }
+            return false
+        }
+        // IDLE
+        else {
+            if string == " " {
+                self.collectionState = .spaceDetected
+            }
             return false
         }
     }
